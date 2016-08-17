bb7e500d6699acb8baa99e8d185e707a94788bd9
Marty Kane <marty@pixotech.com>
2016-08-17 12:40:25 -0500

    made dice input stateless

--- src/components/diceInput.component.js
+++ src/components/diceInput.component.js
@@ -1,7 +1,15 @@
 class DiceInput extends React.Component {
     static get propTypes() {
         return {
-            size: React.PropTypes.number.isRequired
+            number: React.PropTypes.number,
+            size: React.PropTypes.number.isRequired,
+            onDiceChange: React.PropTypes.func
+        };
+    }
+
+    static get defaultProps() {
+        return {
+            number: 1
         };
     }
 
@@ -11,30 +19,29 @@ class DiceInput extends React.Component {
         this.decrementDice = this.decrementDice.bind(this);
         this.incrementDice = this.incrementDice.bind(this);
         this.setDice = this.setDice.bind(this);
-
-        this.state = {
-            dice: 0
-        };
     }
 
-    decrementDice(event) {
-        if (this.state.dice > 0)
-            this.setState({dice: this.state.dice - 1});
+    decrementDice() {
+        this.setDiceState(this.props.number - 1);
     }
 
     incrementDice(event) {
-        this.setState({dice: this.state.dice + 1});
+        this.setDiceState(this.props.number + 1);
     }
 
     setDice(event) {
-        let value = Number(event.target.value);
-        if (value >= 0)
-            this.setState({dice: value});
+        this.setDiceState(Number(event.target.value));
+    }
+
+    setDiceState(number) {
+        if ( number >= 0 ) {
+            this.props.onDiceChange(this.props.size, number);
+        }
     }
 
     render() {
         return <div className="dice-input">
-            <input className="dice-input__number" type="text" name="number" value={ this.state.dice } onChange={ this.setDice }/>
+            <input className="dice-input__number" type="text" name="number" value={ this.props.number } onChange={ this.setDice }/>
             <span className="dice-input__d">d</span>
             <span className="dice-input__size">{ this.props.size }</span>
             <div className="dice-input__controls">
