89feb05537dc2f123ba41354ccbc7959845c8077
Marty Kane <marty@pixotech.com>
2016-08-17 12:40:41 -0500

    Beefed up the dice roller component

--- src/components/diceRoller.component.js
+++ src/components/diceRoller.component.js
@@ -1,13 +1,46 @@
-var DiceRoller = React.createClass({
+class DiceRoller extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.diceChanged = this.diceChanged.bind(this);
+
+        this.state = {
+            diceConfig: [
+                {number: 0, size: 4},
+                {number: 0, size: 6},
+                {number: 0, size: 8},
+                {number: 0, size: 10},
+                {number: 0, size: 12}
+            ]
+        };
+
+        this.diceString = '';
+    }
+
+    diceChanged(size, number) {
+        let newConfig = this.state.diceConfig.map(cfg => {
+            if (cfg.size === size)
+                return {size: cfg.size, number: number};
+            else
+                return {size: cfg.size, number: cfg.number};
+        });
+        this.diceString = newConfig.map(cfg => cfg.number ? `${cfg.number}d${cfg.size} ` : '').join('');
+
+        this.setState({diceConfig: newConfig});
+        console.log(this.diceString);
+    }
+
+    roll() {
+        console.log('roll!');
+    }
+
     render() {
         return (
             <div className="dice-roller">
-                { [4, 6, 8].map(makeDiceInput) }
+                { this.state.diceConfig.map(cfg =>
+                    <DiceInput key={`d${cfg.size}`} number={cfg.number} size={cfg.size} onDiceChange={this.diceChanged}></DiceInput>) }
+                <button className="dice-form__roll-button" type="button" onClick={this.roll}>Roll { this.diceString }</button>
             </div>
         )
     }
-});
-
-function makeDiceInput(size) {
-    return <DiceInput key={`d${size}`} size={size}></DiceInput>
 }
